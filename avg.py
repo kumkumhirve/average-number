dd8402036394ad600cdde623637cacc01d11be3b
kumkumhirve <kumkumhirve452@gmail.com>
2025-02-21 18:48:37 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,7 @@
+def average_of_three(a,b,c):
+    return(a+b+c)/3
+a= int(input("enter the first number:"))
+b= int(input ("enter the second number:"))
+c = int(input("enter the thired number:"))
+average = average_of_three(a,b, c)
+print(average)
